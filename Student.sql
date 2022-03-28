5e78d77bfb44d0b7c2a58be86c71f018c9b78f19
qiuu31 <102507638+qiuu31@users.noreply.github.com>
2022-03-28 14:55:53 +0800

    Add files via upload

---
+++ Student.sql
@@ -0,0 +1,16 @@
+create table assignment1 
+  (id   int primary key,
+   name    varchar(40),
+   department   varchar(40)
+ );
+ 
+ insert into assignment1 (id,name,department) values ('207523',
+'Rusyaidi','FSKTM');
+
+insert into assignment1 (id,name,department) values ('207252',
+'Abdul','FSKTM');
+
+insert into assignment1 (id,name,department) values ('207525',
+'Qayyum','FSKTM');
+
+select * from assignment1;
